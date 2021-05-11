5dcb17ec3f3ebfe25ed4c067e820e72614e54e22
Samirul Alam <samirulalam372@gmail.com>
2021-05-11 22:55:25 +0600

    initialized the project

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD-z9jmxQTzMABFASre4w1W4OSbWjUVxLY",
+    authDomain: "slack-2-9fea9.firebaseapp.com",
+    projectId: "slack-2-9fea9",
+    storageBucket: "slack-2-9fea9.appspot.com",
+    messagingSenderId: "772519644553",
+    appId: "1:772519644553:web:c9f2697ca601439471e558"
+  };
