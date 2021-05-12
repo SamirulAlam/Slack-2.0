627851336ef5290019d80d202b9253dd5801e6fa
Samirul Alam <samirulalam372@gmail.com>
2021-05-12 13:34:58 +0600

    project completed

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyD-z9jmxQTzMABFASre4w1W4OSbWjUVxLY",
     authDomain: "slack-2-9fea9.firebaseapp.com",
@@ -6,3 +8,10 @@ const firebaseConfig = {
     messagingSenderId: "772519644553",
     appId: "1:772519644553:web:c9f2697ca601439471e558"
   };
+
+  const firebaseApp=firebase.initializeApp(firebaseConfig);
+  const db=firebaseApp.firestore();
+  const auth=firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export {auth,provider,db};
